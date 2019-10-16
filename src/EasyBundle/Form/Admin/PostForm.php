4d62666261957b676668cb2e9a1abe1ef3b054ec
Simon Berton <simonberton@gmail.com>
2019-10-16 16:33:03 +0200

    not used class

--- src/EasyBundle/Form/Admin/PostForm.php
+++ src/EasyBundle/Form/Admin/PostForm.php
@@ -5,7 +5,6 @@ namespace App\EasyBundle\Form\Admin;
 
 use App\EasyBundle\Entity\Post;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
